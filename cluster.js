e3930ad5a7adc50a97010448906e0db5f36c4c9a
monai <monai@cure.lt>
2016-03-25 22:21:31 +0200

    Cleanup

--- cluster.js
+++ cluster.js
@@ -16,9 +16,7 @@ if (cluster.isMaster) {
   http.createServer((req, res) => {
     res.writeHead(200, { 'content-type': 'text/plain' });
     res.write('hello world\n');
-    if (cluster.isWorker) {
-      res.write('worker.id: '+ cluster.worker.id);
-    }
+    res.write('worker.id: '+ cluster.worker.id);
     res.end();
   }).listen(3000, () => {
     console.log('Running on http://localhost:3000/');
